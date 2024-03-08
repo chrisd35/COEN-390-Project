d8b5a0307752e547d11061ad6250cff091c7883f
anjxnaa <98621804+anjxnaa@users.noreply.github.com>
2024-03-08 17:01:00 -0500

    Toolbar Added
    Added a toolbar with a back button on sign-up and login pages

--- mainPage/app/src/main/java/com/example/mainpage/loginActivity.java
+++ mainPage/app/src/main/java/com/example/mainpage/loginActivity.java
@@ -1,14 +1,29 @@
 package com.example.mainpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class loginActivity extends AppCompatActivity {
+
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        toolbar = findViewById(R.id.loginPageToolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Back button
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
--- mainPage/app/src/main/java/com/example/mainpage/signUpActivity.java
+++ mainPage/app/src/main/java/com/example/mainpage/signUpActivity.java
@@ -1,14 +1,29 @@
 package com.example.mainpage;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class signUpActivity extends AppCompatActivity {
+
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        toolbar = findViewById(R.id.loginPageToolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Back button
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
