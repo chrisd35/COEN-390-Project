43c59bc9a60750417a818e66da972b23ad2fc3fe
Varun Raval <varun.raval02@gmail.com>
2024-03-18 22:34:21 -0400

    Fixed issues regarding infoPage

---
+++ mainPage/app/src/main/java/com/example/mainpage/infoActivity.java
@@ -0,0 +1,26 @@
+package com.example.mainpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+public class infoActivity extends AppCompatActivity {
+
+
+    protected Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        toolbar = findViewById(R.id.infoPageToolbar);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+}
