954d6ee01da5f1c16431422d30aaa71c7ea8946b
anjxnaa <98621804+anjxnaa@users.noreply.github.com>
2024-03-24 17:51:02 -0400

    Changed UI for Dialog Fragment, created cases and custom statements

--- mainPage/app/src/main/java/com/example/mainpage/DialogFragment.java
+++ mainPage/app/src/main/java/com/example/mainpage/DialogFragment.java
@@ -31,12 +31,73 @@ public class DialogFragment extends androidx.fragment.app.DialogFragment {
 
 
         if(main.isairclicked == true){
-            C02amount.setText("CO2: " + main.getAverageCO2());
-            VOCamount.setText("VOC: " + main.getAverageVOC());
+            C02amount.setText("CO2: " + main.getAverageCO2() + " ppm");
+            VOCamount.setText("VOC: " + main.getAverageVOC() + " ppb");
+
+            if (Integer.valueOf(main.getAverageCO2()) >= 1100 && Integer.valueOf(main.getAverageCO2()) < 1500 ) {
+                alertImage.setImageResource(R.drawable.yellowwarning);
+                level.setText("MEDIOCRE");
+                message.setText("Contaminated indoor air. Ventilation is recommended.");
+            }
+
+            else if (Integer.valueOf(main.getAverageCO2()) >= 1600) {
+                alertImage.setImageResource(R.drawable.redwarning);
+                level.setText("BAD");
+                message.setText("Heavily contaminated indoor air. Ventilation is required.");
+            }
+            else {
+                level.setText("GOOD");
+                message.setText("The air is safe to breathe. No ventilation required. ");
+            }
+
             main.isairclicked = false;
         } else if(main.issoundclicked == true){
-            C02amount.setText("Sound DB " + main.getAverageSound());
+            C02amount.setText("Sound Level: " + main.getAverageSound() + " dB");
             VOCamount.setVisibility(View.GONE);
+
+            // Caution Level
+           if (Integer.valueOf(main.getAverageSound()) >= 70 && Integer.valueOf(main.getAverageSound()) < 80 ) {
+                alertImage.setImageResource(R.drawable.yellowwarning);
+                level.setText("Caution");
+               message.setText("Noise above 70 dB over a prolonged period of time may start to damage your hearing.");
+            }
+           // Red Level 1
+            else if (Integer.valueOf(main.getAverageSound()) >= 80 && Integer.valueOf(main.getAverageSound()) < 85 ) {
+                alertImage.setImageResource(R.drawable.redwarning);
+               level.setText("Level 1");
+                message.setText("Damage to hearing possible after 2 hours of exposure");
+            }
+           // Red Level 2
+           else if (Integer.valueOf(main.getAverageSound()) >= 85 && Integer.valueOf(main.getAverageSound()) < 95 ) {
+               alertImage.setImageResource(R.drawable.redwarning);
+               level.setText("Level 2");
+               message.setText("Damage to hearing possible after about 50 minutes of exposure");
+           }
+           // Red Level 3
+           else if (Integer.valueOf(main.getAverageSound()) >= 95 && Integer.valueOf(main.getAverageSound()) < 100 ) {
+               alertImage.setImageResource(R.drawable.redwarning);
+               level.setText("Level 3");
+               message.setText("Hearing loss possible after 15 minutes");
+           }
+           else if (Integer.valueOf(main.getAverageSound()) >= 100 && Integer.valueOf(main.getAverageSound()) < 110 ) {
+               alertImage.setImageResource(R.drawable.redwarning);
+               level.setText("Level 4");
+               message.setText("Hearing loss possible in less than 5 minutes");
+           }
+           else if (Integer.valueOf(main.getAverageSound()) >= 110 && Integer.valueOf(main.getAverageSound()) < 120 ) {
+               alertImage.setImageResource(R.drawable.redwarning);
+               level.setText("Level 5");
+               message.setText("Hearing loss possible in less than 2 minutes");
+           }
+           else if (Integer.valueOf(main.getAverageSound()) >= 120) {
+               alertImage.setImageResource(R.drawable.redwarning);
+               level.setText("Level 6");
+               message.setText("Immediate pain and injury to the ear");
+           }
+           else {
+               message.setText("You're within the safe dB range. ");
+               level.setText("Safe Level");
+           }
             main.issoundclicked = false;
         }
 
